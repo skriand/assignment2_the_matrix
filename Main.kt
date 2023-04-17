06140f49a47a630d7a71bad566ed83fa1c4c07a8
skriand <andrey.skri2011@gmail.com>
2023-04-17 03:13:10 +0300

    Update

--- Main.kt
+++ Main.kt
@@ -1,8 +1,99 @@
-@file:Suppress("UNUSED_PARAMETER")
-package mmcs.assignment2
+/**
+ * Ячейка матрицы: row = ряд, column = колонка
+ */
+data class Cell(val row: Int, val column: Int)
 
-import mmcs.assignment2.Matrix
-import mmcs.assignment2.createMatrix
+/**
+ * Интерфейс, описывающий возможности матрицы. E = тип элемента матрицы
+ */
+interface Matrix<E> {
+    /** Высота */
+    val height: Int
+
+    /** Ширина */
+    val width: Int
+
+    /**
+     * Доступ к ячейке.
+     * Методы могут бросить исключение, если ячейка не существует или пуста
+     */
+    operator fun get(row: Int, column: Int): E
+
+    operator fun get(cell: Cell): E
+
+    /**
+     * Запись в ячейку.
+     * Методы могут бросить исключение, если ячейка не существует
+     */
+    operator fun set(row: Int, column: Int, value: E)
+
+    operator fun set(cell: Cell, value: E)
+}
+
+/**
+ * Метод для создания матрицы, должен вернуть РЕАЛИЗАЦИЮ Matrix<E>.
+ * height = высота, width = ширина, e = чем заполнить элементы.
+ * Бросить исключение IllegalArgumentException, если height или width <= 0.
+ */
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> {
+    if (height <= 0 || width <= 0 )
+        throw IllegalArgumentException("Matrix can't be create")
+    var res = MatrixImpl<E>(height, width, e)
+
+    return res
+}
+
+/**
+ * Реализация интерфейса "матрица"
+ */
+
+@Suppress("EqualsOrHashCode")
+class MatrixImpl<E>(override val height: Int, override val width: Int, private val e: MutableList<E>) : Matrix<E> {
+
+    constructor(height: Int, width: Int, e: E) : this(height, width, MutableList(height * width) { e })
+
+    override fun get(row: Int, column: Int): E =
+        e[row*height + column] ?: throw IllegalArgumentException("Out of bounds")
+
+    override fun get(cell: Cell): E = get(cell.row, cell.column)
+
+    override fun set(row: Int, column: Int, value: E) {
+        if (column > width - 1 || row > height - 1)
+            throw IllegalArgumentException("Out of bounds")
+        e[row*height + column] = value
+    }
+
+    override fun set(cell: Cell, value: E) = set(cell.row, cell.column, value)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+
+        if (other !is MatrixImpl<*>) return false
+        if (height != other.height || width != other.width)
+            return false
+        for (row in 0 until height) {
+            for (column in 0 until width) {
+                if (get(row, column) != other[row, column])
+                    return false
+            }
+        }
+
+        return true
+    }
+
+    override fun toString(): String {
+        var res = StringBuilder()
+        for (row in 0 until height) {
+            for (column in 0 until width) {
+                res.append(get(row, column))
+                res.append(" ")
+            }
+            if (row != height - 1)
+                res.append("\n")
+        }
+        return res.toString()
+    }
+}
 
 /**
  * Пример
@@ -153,3 +244,33 @@ data class Holes(val rows: List<Int>, val columns: List<Int>)
  * Если наложение невозможно, то первый элемент тройки "нет" и сдвиги могут быть любыми.
  */
 fun canOpenLock(key: Matrix<Int>, lock: Matrix<Int>): Triple<Boolean, Int, Int> = TODO()
+
+fun main() {
+
+    println("Создание матрицы")
+    val mat = createMatrix(3,3,1)
+    mat.set(Cell(0,1),2)
+    mat.set(2,2,3)
+    println(mat)
+     
+    println("Транспонирование")
+    println(transpose(mat))
+        
+    println("Поворот")
+    println(rotate(mat))
+   
+    println("Сложение")
+    println(mat.plus(rotate(mat)))
+   
+    println("Инверсия")
+    println(mat.unaryMinus())
+
+	println("Умножение")
+    println(mat.times(createMatrix(3,3,1)))
+   
+    println("Дырки")
+    val other_mat = createMatrix(3,3,0)
+    other_mat.set(0,2,1)
+    println(other_mat)
+    println(findHoles(other_mat))
+}
--- Matrix.kt
+++ Matrix.kt
@@ -38,32 +38,62 @@ interface Matrix<E> {
  * height = высота, width = ширина, e = чем заполнить элементы.
  * Бросить исключение IllegalArgumentException, если height или width <= 0.
  */
-fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = TODO()
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> {
+    if (height <= 0 || width <= 0 )
+        throw IllegalArgumentException("Matrix can't be create")
+    var res = MatrixImpl<E>(height, width, e)
+
+    return res
+}
 
 /**
  * Реализация интерфейса "матрица"
  */
 
 @Suppress("EqualsOrHashCode")
-class MatrixImpl<E> : Matrix<E> {
+class MatrixImpl<E>(override val height: Int, override val width: Int, private val e: MutableList<E>) : Matrix<E> {
 
-    override val height: Int = TODO()
+    constructor(height: Int, width: Int, e: E) : this(height, width, MutableList(height * width) { e })
 
-    override val width: Int = TODO()
+    override fun get(row: Int, column: Int): E =
+        e[row*height + column] ?: throw IllegalArgumentException("Out of bounds")
 
-    override fun get(row: Int, column: Int): E = TODO()
-
-    override fun get(cell: Cell): E = TODO()
+    override fun get(cell: Cell): E = get(cell.row, cell.column)
 
     override fun set(row: Int, column: Int, value: E) {
-        TODO()
+        if (column > width - 1 || row > height - 1)
+            throw IllegalArgumentException("Out of bounds")
+        e[row*height + column] = value
     }
 
-    override fun set(cell: Cell, value: E) {
-        TODO()
+    override fun set(cell: Cell, value: E) = set(cell.row, cell.column, value)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+
+        if (other !is MatrixImpl<*>) return false
+        if (height != other.height || width != other.width)
+            return false
+        for (row in 0 until height) {
+            for (column in 0 until width) {
+                if (get(row, column) != other[row, column])
+                    return false
+            }
+        }
+
+        return true
     }
 
-    override fun equals(other: Any?) = TODO()
-
-    override fun toString(): String = TODO()
+    override fun toString(): String {
+        var res = StringBuilder()
+        for (row in 0 until height) {
+            for (column in 0 until width) {
+                res.append(get(row, column))
+                res.append(" ")
+            }
+            if (row != height - 1)
+                res.append("\n")
+        }
+        return res.toString()
+    }
 }
